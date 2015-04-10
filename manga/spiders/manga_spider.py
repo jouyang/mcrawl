b825fde61fdaa1a347f907163a62b6c0a567d401
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2015-04-10 01:58:36 -0700

    Initialize

---
+++ manga/spiders/manga_spider.py
@@ -0,0 +1,32 @@
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+
+from manga.items import MangaItem
+
+
+class mangaSpider(Spider):
+    name = "manga"
+    allowed_domains = ["manhua.dmzj.com"]
+    start_urls = [
+        "manhua.dmzj.com"
+    ]
+
+    def parse(self, response):
+        """
+        The lines below is a spider contract. For more info see:
+        http://doc.scrapy.org/en/latest/topics/contracts.html
+        @url http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/
+        @scrapes name
+        """
+        sel = Selector(response)
+        sites = sel.xpath('//ul[@class="directory-url"]/li')
+        items = []
+
+        for site in sites:
+            item = Website()
+            item['name'] = site.xpath('a/text()').extract()
+            item['url'] = site.xpath('a/@href').extract()
+            item['description'] = site.xpath('text()').re('-\s[^\n]*\\r')
+            items.append(item)
+
+        return items
